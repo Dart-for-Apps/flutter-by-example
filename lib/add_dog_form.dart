cecded3e7a7ef5b3b7050b5e2da08d0195b04f4a
wurikiji <wurikiji@gmail.com>
2019-01-24 03:06:04 +0900

    Add dog forms and routine to deal with returned data

--- lib/add_dog_form.dart
+++ lib/add_dog_form.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'dog_model.dart';
+
 class AddDogForm extends StatefulWidget {
   @override
   _AddDogFormState createState() => _AddDogFormState();
 }
 
 class _AddDogFormState extends State<AddDogForm> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +28,7 @@ class _AddDogFormState extends State<AddDogForm> {
               Padding(
                 padding: const EdgeInsets.only(bottom: 8.0),
                 child: TextField(
+                  controller: nameController,
                   decoration: InputDecoration(
                     labelText: 'Name the Pup',
                   ),
@@ -30,6 +37,7 @@ class _AddDogFormState extends State<AddDogForm> {
               Padding(
                 padding: const EdgeInsets.only(bottom: 8.0),
                 child: TextField(
+                  controller: locationController,
                   decoration: InputDecoration(
                     labelText: "Pup's location",
                   ),
@@ -38,6 +46,7 @@ class _AddDogFormState extends State<AddDogForm> {
               Padding(
                 padding: const EdgeInsets.only(bottom: 8.0),
                 child: TextField(
+                  controller: descController,
                   decoration: InputDecoration(
                     labelText: 'All about the Pup',
                   ),
@@ -45,14 +54,11 @@ class _AddDogFormState extends State<AddDogForm> {
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Builder(
-                  builder: (BuildContext _context) {
-                    return RaisedButton(
-                      onPressed: () {},
-                      color: Colors.indigoAccent,
-                      child: Text('Submit Pup'),
-                    );
-                  },
+                // 잘못된 context 사용 예제 제거
+                child: RaisedButton(
+                  onPressed: () => _submitPup(context),
+                  color: Colors.indigoAccent,
+                  child: Text('Submit Pup'),
                 ),
               ),
             ],
@@ -60,6 +66,15 @@ class _AddDogFormState extends State<AddDogForm> {
         ),
       ),
     );
+  }
+
+  void _submitPup(BuildContext context) {
+    if (nameController.text.isEmpty) {
+      print("Dogs need names!");
+    } else {
+      Dog newDog = Dog(nameController.text, locationController.text, descController.text);
+      Navigator.of(context).pop(newDog);
+    }
   }
 }
 
